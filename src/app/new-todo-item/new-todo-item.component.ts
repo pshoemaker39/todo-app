4dac92e041679ac4af41541001446d3fd7df97ab
Price Shoemaker <priceshoemaker@Prices-MacBook-Pro.local>
2019-10-28 21:13:30 -0500

    initial files

---
+++ src/app/new-todo-item/new-todo-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-new-todo-item',
+  templateUrl: './new-todo-item.component.html',
+  styleUrls: ['./new-todo-item.component.scss']
+})
+export class NewTodoItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
